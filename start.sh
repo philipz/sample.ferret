06ca5937592910cb4a3d71c2bfd8687d8b74886a
philipz <philipzheng@gmail.com>
2022-06-04 14:39:08 +0800

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker run -d -p 80:9080 -p 443:9443 \
-	    -v $(pwd)/targer/ferret-1.3-SNAPSHOT.war:/config/dropins/ferret-1.3-SNAPSHOT.war \
-	    websphere-liberty:22.0.0.5-kernel-java11-openj9
+	    -v $(pwd)/target/ferret-1.3-SNAPSHOT.war:/config/dropins/ferret-1.3-SNAPSHOT.war \
+	    websphere-liberty:22.0.0.5-full-java11-openj9
